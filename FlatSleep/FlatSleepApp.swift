05e10234990124d39a1877acb9e96127ce0120bc
Radomyr Sidenko <radik-sidehnko@ukr.net>
2026-05-26 13:42:38 +0300

    Initial Commit

---
+++ FlatSleep/FlatSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlatSleepApp.swift
+//  FlatSleep
+//
+//  Created by Radomyr Sidenko on 26.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct FlatSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
